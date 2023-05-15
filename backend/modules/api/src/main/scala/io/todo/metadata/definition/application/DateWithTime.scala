2c4cdafeff41eb798ff1aa6f002d0255acb517e5
DominikKunadt <dominikkunadt@gmail.com>
2023-05-15 12:45:33 +0200

    add openapi.json

--- backend/modules/api/src/main/scala/io/todo/metadata/definition/application/DateWithTime.scala
+++ backend/modules/api/src/main/scala/io/todo/metadata/definition/application/DateWithTime.scala
@@ -9,12 +9,9 @@ import smithy4s.Newtype
 
 object DateWithTime extends Newtype[String] {
   val id: ShapeId = ShapeId("io.todo.metadata.definition.application", "DateWithTime")
-  val hints: Hints = Hints(
-    smithy.api.Documentation("ISO Date With Time")
+  val hints : Hints = Hints(
+    smithy.api.Documentation("ISO Date With Time"),
   )
-  val underlyingSchema: Schema[String] = string
-    .withId(id)
-    .addHints(hints)
-    .validated(smithy.api.Pattern("^\\d{4}-\\d\\d-\\d\\dT\\d\\d:\\d\\d:\\d\\d(\\.\\d+)?(([+-]\\d\\d:\\d\\d)|Z)?$"))
-  implicit val schema: Schema[DateWithTime] = bijection(underlyingSchema, asBijection)
+  val underlyingSchema : Schema[String] = string.withId(id).addHints(hints).validated(smithy.api.Pattern("^\\d{4}-\\d\\d-\\d\\dT\\d\\d:\\d\\d:\\d\\d(\\.\\d+)?(([+-]\\d\\d:\\d\\d)|Z)?$"))
+  implicit val schema : Schema[DateWithTime] = bijection(underlyingSchema, asBijection)
 }
